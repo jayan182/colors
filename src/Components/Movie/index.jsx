62626ad0ca2d55a51183df941392f659df6e2fa7
Jayan Kapali <jayan.kapali@auxfin.com>
2023-05-03 17:44:20 +0545

    features: remove console

--- src/Components/Movie/index.jsx
+++ src/Components/Movie/index.jsx
@@ -96,24 +96,6 @@ function Index(){
             {
                 Object.values(data).length > 0 &&
                     <div style={styles.container}>
-                        {/* <Pagination>
-                            <Pagination.First />
-                            <Pagination.Prev />
-                            <Pagination.Item>{1}</Pagination.Item>
-                            <Pagination.Ellipsis />
-
-                            <Pagination.Item>{10}</Pagination.Item>
-                            <Pagination.Item>{11}</Pagination.Item>
-                            <Pagination.Item active>{12}</Pagination.Item>
-                            <Pagination.Item>{13}</Pagination.Item>
-                            <Pagination.Item disabled>{14}</Pagination.Item>
-
-                            <Pagination.Ellipsis />
-                            <Pagination.Item>{20}</Pagination.Item>
-                            <Pagination.Next />
-                            <Pagination.Last />
-                        </Pagination> */}
-
                         <Pagination>
                             {
                                 [0,1,2,3,4].map((tmp)=>{
